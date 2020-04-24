9ed24fece49e97fc893fbb0aade8c38e343f8406
Scott Davis <scott@thirstyhead.com>
2020-04-24 14:01:26 -0600

    started browser chapter

---
+++ src/book/browser/opening_and_closing_a_browser.js
@@ -0,0 +1,10 @@
+const { openBrowser, closeBrowser } = require('taiko');
+(async () => {
+    try {
+        await openBrowser();
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await closeBrowser();
+    }
+})();
