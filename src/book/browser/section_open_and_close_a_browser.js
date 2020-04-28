8f7656bd7d2c0f2ad83050cb3cdaca61eae21104
Scott Davis <scott@thirstyhead.com>
2020-04-28 12:34:51 -0600

    section script window position

--- src/book/browser/section_open_and_close_a_browser.js
+++ src/book/browser/section_open_and_close_a_browser.js
@@ -1,12 +1,11 @@
 const { openBrowser, closeBrowser } = require('taiko');
-(async () => {
-    try {
-        // await openBrowser();
+(async () => {                    
+    try {                       
         await openBrowser({args:['--window-size=1024,768', '--window-position=2048,0']});
-        // await screenshot({path : 'opening_and_closing_a_browser.png'})
     } catch (error) {
         console.error(error);
     } finally {
         await closeBrowser();
     }
 })();
+
