e580371afbc6c39dd1b23381555dca43a073c78e
Scott Davis <scott@thirstyhead.com>
2020-04-30 10:41:45 -0600

    working on automated video capture of Taiko actions

---
+++ src/book/__notes/macos-screen-recording.scpt
@@ -0,0 +1,4 @@
+tell application "QuickTime Player"
+     activate
+     start (new screen recording)
+ end tell
--- src/book/browser/section_open_and_close_a_browser.js
+++ src/book/browser/section_open_and_close_a_browser.js
@@ -1,7 +1,9 @@
 const { openBrowser, closeBrowser } = require('taiko');
 (async () => {                    
     try {                       
-        await openBrowser({args:['--window-size=1024,768', '--window-position=2048,0']});
+        await openBrowser({args:['--window-size=1024,768', 
+                                 '--window-position=2048,0']});
+        screenshot()
     } catch (error) {
         console.error(error);
     } finally {
