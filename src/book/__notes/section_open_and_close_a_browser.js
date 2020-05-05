e285da5bc42414d8949e7ab2d5a88746ab168404
Scott Davis <scott@thirstyhead.com>
2020-05-05 14:02:29 -0600

    code examples split out into In the REPL and In a script

---
+++ src/book/__notes/section_open_and_close_a_browser.js
@@ -0,0 +1,13 @@
+const { openBrowser, closeBrowser } = require('taiko');
+(async () => {                    
+    try {                       
+        await openBrowser({args:['--window-size=1024,768', 
+                                 '--window-position=2048,0']});
+        screenshot()
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await closeBrowser();
+    }
+})();
+
--- src/book/browser/section_open_and_close_a_browser.js
+++ src/book/browser/section_open_and_close_a_browser.js
@@ -1,9 +1,7 @@
 const { openBrowser, closeBrowser } = require('taiko');
 (async () => {                    
     try {                       
-        await openBrowser({args:['--window-size=1024,768', 
-                                 '--window-position=2048,0']});
-        screenshot()
+        await openBrowser()
     } catch (error) {
         console.error(error);
     } finally {
